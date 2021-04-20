2ec054fb9d4db368c3f14237db21f712bc8c7832
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-04-20 22:37:59 +0300

    Task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,24 @@
+'use strict';
+// Напиши скрипт, который выполнит следующие операции.
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+// Например для первой категории получится:
+// Категория: Животные
+// Количество элементов: 4
+
+// Находим по ИД нужный узел и выбираем его
+const categories = document.querySelector('ul#categories');
+
+// Находим всё количество елементов в нужном узле
+const findAllCategories = categories.querySelectorAll('li.item');
+
+console.log(`В списке ${findAllCategories.length} категории`);
+
+// Перебираем каждый элемент, находим его заголовок (текстовый контент) количество внутренних елементов(длинна массива) и  выводим консоль.
+findAllCategories.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryItems = item.querySelectorAll('li').length;
+
+  console.log(`Категория: ${categoryTitle}`);
+  console.log(`Количество элементов: ${categoryItems}`);
+});
