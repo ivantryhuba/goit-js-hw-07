a6027edfd7911fe0f02587850590a0c2a438ade3
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-04-23 23:00:31 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -4,3 +4,38 @@
 // !Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // !Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // !Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+// Находим блок счетчика
+const findCounter = document.querySelector('#counter');
+
+// Создаём объект из елементов счётчика
+const refs = {
+  value: findCounter.querySelector('#value'),
+  decrementBtn: findCounter.querySelector('[data-action="decrement"]'),
+  incrementBtn: findCounter.querySelector('[data-action="increment"]'),
+};
+
+// Создаем переменную для хранения данных счётчика
+let counterValue = 0;
+
+// Проверяем что насоздавали
+console.log(refs.value);
+console.log(refs.decrementBtn);
+console.log(refs.incrementBtn);
+console.log(counterValue);
+
+// Создаем функции с которих возвращаем текстовый контент счетчика
+//* Функция декремента(-)
+const decrement = () => {
+  counterValue -= 1;
+  return (refs.value.textContent = counterValue);
+};
+//* Функция инкремента(+)
+const increment = () => {
+  counterValue += 1;
+  return (refs.value.textContent = counterValue);
+};
+
+// Вешаем слушатели событий на кнопки
+refs.decrementBtn.addEventListener('click', decrement);
+refs.incrementBtn.addEventListener('click', increment);
